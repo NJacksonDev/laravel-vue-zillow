df5bcd0cc7a1c9ee792079226a8606e5ddd9cf7c
Nick Jackson <nickdjackson@msn.com>
2023-04-01 14:54:21 -0400

    add layout

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>The message is {{ message }}</div>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>The message is {{ message }}</div>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-defineProps(["message"]);
+import MainLayout from "../../Layouts/MainLayout.vue";
+defineProps({
+    message: String,
+});
 </script>
